0632c35ee33dac19b4261c1549e2831963499ef5
Wyson Kong <wyson.kong.mil@aswf.army.mil>
2025-08-22 10:43:55 -0500

    spring added gradle build

---
+++ src/main/java/com/example/pizza_repo/PizzaRepoApplication.java
@@ -0,0 +1,13 @@
+package com.example.pizza_repo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaRepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzaRepoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pizza_repo/PizzaRepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pizza_repo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzaRepoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
